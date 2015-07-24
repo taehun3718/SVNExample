c0d59324e3512323cac14c2d6bb110723bdb4dd5
KTDS_Christof_Kim <taehun3718@gmail.com>
2015-07-24 10:20:12 +0900

    저자 적으면 에러남
    왜그러는지 모르겠음..

--- app/src/main/java/com/ktds/githubexample/MainActivity.java
+++ app/src/main/java/com/ktds/githubexample/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //으아아아
+        //으아아아ㅋㅋ
     }
 
     @Override
